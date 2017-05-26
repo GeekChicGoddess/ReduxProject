83a7de689a43260178d055f665dcacf9dadce855
melodytempleton <mtempletoncantu@gmail.com>
2017-05-25 19:35:08 -0500

    Hook up state/props, Redux/React in book-list.js

--- src/container/book-list.js
+++ src/container/book-list.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookList extends Component {
 
+class BookList extends Component {
 
     renderList(){
        return this.props.books.map((book) => {
@@ -21,3 +22,10 @@ export default class BookList extends Component {
     }
 
 }
+
+    function mapStateToProps(state){
+        return{
+          books: state.books
+        };
+}
+ export default connect(mapStateToProps)(BookList);
